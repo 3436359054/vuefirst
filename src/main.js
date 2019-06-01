d1d78d02ef82ef3f85a559001d4459dfd1fb05a7
test <stfen@stfen.local>
2019-06-01 10:54:46 +0800

    首页开头

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import "@/assets/css/reset.css"
+import "@/assets/css/iconfont/iconfont.css"
 import FastClick from "fastclick"
 FastClick.attach(document.body);
 Vue.config.productionTip = false
--- src/pages/index/header.vue
+++ src/pages/index/header.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="header">
-    我是头部  
+    <div class="back iconfont">&#xe632;</div>
+    <div class="seach"></div>
+    <div class="point"></div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'IndexHeader'
-  }
+export default {
+  name: 'IndexHeader'
+}
 </script>
 
 <style lang="stylus" scoped>
   .header
     display:flex
-    height 0.88rem
-    
+    height:0.88rem
+    background: #03bbd4
+    .back
+      width:.6rem
 </style>
